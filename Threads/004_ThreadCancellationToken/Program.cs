8dcd2742f15aa29f42d62177d25cd6cbf2d1ac42
Razmik <Razmik@DESKTOP-UKL3EO5>
2023-11-20 17:46:31 +0400

    CancellationToken

---
+++ Threads/004_ThreadCancellationToken/Program.cs
@@ -0,0 +1,32 @@
+﻿public class CancellationTokenProgram
+{
+    public static void Main()
+    {
+        CancellationTokenSource cts = new CancellationTokenSource();
+
+        //Pass the CancellationToken and the number-to-count into the operation
+        ThreadPool.QueueUserWorkItem(o => Count(cts.Token, 1000));
+
+        Thread.Sleep(3000);
+        cts.Cancel();
+
+        Console.ReadLine();
+    }
+
+    private static void Count(CancellationToken token, int countTo)
+    {
+        for (int i = 0; i < countTo; i++)
+        {
+            if (token.IsCancellationRequested)
+            {
+                Console.WriteLine("Count is cancelled");
+                break; // Exit the loop to stop the operation
+            }
+
+            Console.WriteLine(i);
+            Thread.Sleep(200); // For demo, waste some time
+        }
+
+        Console.WriteLine("Count is done");
+    }
+}
