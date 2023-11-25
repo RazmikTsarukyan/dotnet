b1cc3566f932793cb0260910abc08cb06f45dba4
Razmik Tsarukyan <razmiktsarukyan@gmail.com>
2023-11-25 11:03:56 +0400

    void Task

--- Tasks/002_Tasks/Program.cs
+++ Tasks/002_Tasks/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("002_Tasks");
+﻿public class VoidTaskProgram
+{
+    public static void Main()
+    {
+        Task t1 = new Task(() => SayHi());
+        Task t2 = new Task((person) => SayHi(((Person)person).FirstName, ((Person)person).LastName), new Person() { FirstName = "Jhon", LastName = "Smit" });
+
+        t1.Start();
+        t2.Start();
+
+        t1.Wait();
+        t2.Wait();
+    }
+
+    private static void SayHi()
+    {
+        Console.WriteLine("Hi");
+    }
+
+    public static void SayHi(string firstname, string lastname)
+    {
+        Thread.Sleep(2000);
+        Console.WriteLine($"Hi {firstname} {lastname}");
+    }
+}
+
+public class Person
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+}
