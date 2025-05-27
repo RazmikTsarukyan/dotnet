88106a9527e8e0f2860f057b6d748209d8b194d2
Razmik <Razmik@DESKTOP-UKL3EO5>
2025-05-27 13:17:03 +0400

    add 008_Threads_Synchronization_Monitor

---
+++ Threads/008_Threads_Synchronization_Monitor/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    static int counter;
+    static object counterLock = new object();
+    public static void Main()
+    {
+        Thread thread1 = new Thread(IncrementCounter);
+        Thread thread2 = new Thread(IncrementCounter);
+
+        thread1.Start();
+        thread2.Start();
+
+        thread1.Join();
+        thread2.Join();
+
+        Console.WriteLine($"Final counter value is:{counter}");
+    }
+
+    static void IncrementCounter()
+    {
+        for (int i = 0; i < 1000000; i++) 
+        {
+            Monitor.Enter(counterLock);
+            try
+            {
+                counter = counter + 1;
+            }
+            finally
+            {
+                Monitor.Exit(counterLock);
+            }
+        }
+    }
+}
+
+//The lock statement is a language-level shortcut (syntactic sugar) for using Monitor.Enter and Monitor.Exit.
+//Instead of lock compiler generates code like IncrementCounter's monitor section with Monitor.Enter, try finally and Monitor.Exit.
